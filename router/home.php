7a3e078d285649371d360e7bf0ba47f4713849d7
QuentiumYT <quentin.lienhardt@gmail.com>
2020-12-08 13:05:02 +0100

    Fixed typo and date

--- router/home.php
+++ router/home.php
@@ -1,4 +1,4 @@
-<!-- Yeah this page is basicly only HTML -->
+<!-- Yeah this page is basically only HTML -->
 <div class="banner">
     <h2>Bienvenue</h2>
 </div>
@@ -7,7 +7,7 @@
     <p class="introduction">Hey, bienvenue sur le WikiWorld ! Ce site web répertorie toutes les voitures iconiques du jeu
         Need For Speed : World ainsi que toutes leurs éditions respectives et leur constructeurs.</p>
     <div class="about">
-        <p>Le jeu Need For Speed : World est le 15ème jeu de la série sorti le 27 Juillet 2020 développé par EA.
+        <p>Le jeu Need For Speed : World est le 15ème jeu de la série sorti le 27 Juillet 2010 développé par EA.
             Ce fut le tout premier jeu Need For Speed gratuit multijoueur. Le jeu est devenu hors ligne le 14 Juillet 2015
             dû au manque de joueurs suffisants pour EA.
             Il existe encore des serveurs non-officiels maintenus pour pouvoir continuer à jouer au jeu et garder tout ces
